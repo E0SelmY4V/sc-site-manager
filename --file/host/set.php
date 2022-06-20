9454aec82de549d583bc02ec8cfe5ccb9918bf02
E0SelmY4V-PC <2507917746@qq.com>
2022-06-21 00:59:02 +0800

    fix: 修改了配置文件存放的位置问题

--- --file/host/set.php
+++ --file/host/set.php
@@ -8,10 +8,10 @@ function diff($bag, $check)
 $old_list = json_decode(file_get_contents('list.json'), true);
 file_put_contents('list.json', $new_list = urldecode($_SERVER['QUERY_STRING']));
 $new_list = json_decode($new_list, true);
-foreach (diff($old_list, $new_list) as $dmn => $info) unlink("cfg/$dmn.conf");
+foreach (diff($old_list, $new_list) as $dmn => $info) unlink("../cfg/host-$dmn.conf");
 foreach (diff($new_list, $old_list) as $dmn => $info) {
 	$text = '<VirtualHost *:' . PORT . ">\n\tServerName $dmn";
 	foreach ($info as $key => $value) $text .= "\n\t$key " . ($key === 'DocumentRoot' ? ROOT : '') . $value;
 	$text .= "\n</VirtualHost>";
-	file_put_contents("cfg/$dmn.conf", $text);
+	file_put_contents("../cfg/host-$dmn.conf", $text);
 }
